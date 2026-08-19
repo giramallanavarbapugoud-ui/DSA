1bfd9528e7280fd8d928bc56a8304751b78ffab6
giramallanavarbapugoud-ui <giramallanavarbapugoud@gmail.com>
2026-08-19 22:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (92.84%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int i=s.length()-1;
+        int count=0;
+        
+        while(s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            count+=1;
+            i--;
+        }
+        return count;
+
+        
+    }
+};
